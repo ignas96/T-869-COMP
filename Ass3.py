31e51b43f085e19f8054dd1617a394824e5bbfd5
ignas <ignas18@ru.is>
2022-11-28 17:08:06 +0000

    Structure

--- Ass3.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-import random
-import cv2 as cv
-import matplotlib.pyplot as plt
-
-
---
+++ Verkefni 3/Ass3.py
@@ -0,0 +1,14 @@
+import numpy as np
+import random
+import cv2 
+import matplotlib.pyplot as plt
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    vid = cv2.VideoCapture(0)
+
+    main()
+
+
